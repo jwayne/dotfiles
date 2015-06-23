5c68ef16fb59a67d6ea59e25c93e8b1c0009f371
Josh Chen <jwcstar@gmail.com>
2015-06-22 23:37:12 -0400

    add spacemacs, make everything an option

--- install.sh
+++ install.sh
@@ -4,120 +4,216 @@
 # This script does all the install steps.
 ############################
 
-#-----
-# Handle args
+#TODO: spacemacs from nikki93, tmux
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    is_mac=true
+    is_linux=
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    is_mac=
+    is_linux=true
+else
+    is_mac=
+    is_linux=
+fi
+
+#-----
+# Handle args
+
 # Defaults
-setup_zsh=true  #if false, we use bash
-setup_git=
+arg_programs=
+arg_shell=
+arg_dotfiles=
+arg_git=
+
+print_help() {
+    echo "--------------------------------------------------------------------------------"
+    echo "$0 [options]"
+    echo
+    echo "This program sets up your environment for you."
+    echo "You have to provide arguments for what you want it to set up."
+    echo "By default, it will not install anything."
+    echo
+    echo "options:"
+    echo "-h, --help                show brief help"
+    echo "--all                     install all below options"
+    echo "--programs                default: no install"
+    echo "--shell zsh|bash          default: no install; if all then zsh"
+    echo "--dotfiles                default: no install"
+    echo "--git                     default: no install"
+    exit 0
+}
 
 while test $# -gt 0; do
     case "$1" in
         -h|--help)
-            echo "$package - install environment"
-            echo " "
-            echo "$package [options] [arguments]"
-            echo " "
-            echo "options:"
-            echo "-h, --help                show brief help"
-            echo "--bash OR --zsh"
-            echo "--git"
-            exit 0
+            print_help
             ;;
-        --bash)
-            setup_zsh=
+
+        --all)
+            arg_shell="zsh"
+            arg_programs=true
+            arg_dotfiles=true
+            arg_git=true
             shift
             ;;
-        --zsh)
-            setup_zsh=true            
+
+        --programs)
+            arg_programs=true
             shift
             ;;
+
+        --shell)
+            shift
+            arg_shell="$1"
+            if [ "$arg_shell" != "zsh" ] && [ "$arg_shell" != "bash" ]; then
+                echo "Unknown --shell: $arg_shell"
+                print_help
+                exit 1
+            fi
+            shift
+            ;;
+
+        --dotfiles)
+            arg_dotfiles=true
+            shift
+            ;;
+
         --git)
-            setup_git=true            
+            arg_git=true            
             shift
             ;;
+
+        *)
+            echo "Unknown option or argument: $1"
+            print_help
+            exit 1
     esac
 done
-        
+
+#-----
+# program installs
+
+if [ "$arg_programs" = true ]; then
+    if [ $is_mac = true ]; then
+        echo "================================================================================"
+        echo "OS X program installs"
+        echo "--------------------------------------------------------------------------------"
+
+        echo "installing Homebrew"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        echo -e "done\n"
+
+        echo "installing Spacemacs"
+        brew tap railwaycat/emacsmacport
+        brew install emacs-mac
+        echo -e "done\n"
+
+        echo "You should install these apps manually:"
+        echo "ShiftIt"
+        echo "Press enter to continue..."
+        read -e
+    elif [ $is_linux = true ]; then
+        echo "================================================================================"
+        echo "Linux program installs"
+        echo "--------------------------------------------------------------------------------"
+
+        echo "installing Spacemacs"
+        git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
+        echo -e "done\n"
+    fi
+fi       
+
 #-----
 # zsh or bash?
 
-echo "================================================================================"
-echo "setting up your shell"
-echo "--------------------------------------------------------------------------------"
-if [ "$setup_zsh" = true ] && [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    echo "using zsh"
-    echo
-    if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        echo "installing oh-my-zsh"
-        sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-        echo
-    else
-        echo "oh-my-zsh already installed"
-        echo
-        if [ "$SHELL" != "/bin/zsh" ]; then
-            echo "chsh -s /bin/zsh"
-            chsh -s /bin/zsh
-            set SHELL=/bin/zsh
+if [ ! -z "$arg_shell" ]; then
+    echo "================================================================================"
+    echo "setting up your shell"
+    echo "--------------------------------------------------------------------------------"
+    if [ "$arg_shell" == "zsh" ]; then
+        if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+            echo "using zsh"
+            echo
+            if [ ! -d "$HOME/.oh-my-zsh" ]; then
+                echo "installing oh-my-zsh"
+                sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+                echo
+            else
+                echo "oh-my-zsh already installed"
+                echo
+                if [ "$SHELL" != "/bin/zsh" ]; then
+                    echo "chsh -s /bin/zsh"
+                    chsh -s /bin/zsh
+                    set SHELL=/bin/zsh
+                else
+                    echo "default shell is already zsh, nothing to do"
+                    echo
+                fi
+            fi
+            echo "cp $dir/dotfiles/.bashrc $HOME/.oh-my-zsh/custom/$USER.zsh"
+            cp $dir/dotfiles/.bashrc $HOME/.oh-my-zsh/custom/$USER.zsh
         else
-            echo "default shell is already zsh, nothing to do"
+            echo "zsh not available, using bash instead"
+            arg_shell="bash"
+        fi
+    fi
+    if [ "$arg_shell" == "bash" ]; then
+        echo "using bash"
+        echo
+        if [ "$SHELL" != "/bin/bash" ]; then
+            echo "chsh -s /bin/bash"
+            chsh -s /bin/bash
+            set SHELL=/bin/bash
+        else
+            echo "default shell is already bash, nothing to do"
             echo
         fi
     fi
-else
-    echo "using bash (either you chose this option, or zsh wasn't available"
-    echo
-    if [ "$SHELL" != "/bin/bash" ]; then
-        echo "chsh -s /bin/bash"
-        chsh -s /bin/bash
-        set SHELL=/bin/bash
-    else
-        echo "default shell is already bash, nothing to do"
-        echo
-    fi
+    echo -e "done\n"
 fi
-echo -e "done\n"
 
 #-----
 # dotfiles
 
-echo "================================================================================"
-echo "installing dotfiles"
-echo "--------------------------------------------------------------------------------"
-dir_dotfiles=$dir/dotfiles
-dir_dotfiles_old=$dir_dotfiles/old
-dotfiles="$(ls -A $dir_dotfiles)"
-if [ ! -d $dir_dotfiles_old ]; then
-    echo "mkdir $dir_dotfiles_old"
-    mkdir $dir_dotfiles_old
-    echo 
-fi
-for file in $dotfiles; do
-    if [ "$file" != "old" ]; then
-        # If existing symlink
-        if [ -h ~/$file ]; then
-            echo "rm ~/$file"
-            rm ~/$file
-        # If existing regular file
-        elif [ -e ~/$file ]; then
-            echo "mv ~/$file $dir_dotfiles_old/"
-            mv ~/$file $dir_dotfiles_old/
-        fi
-        # Now, make the symlink
-        echo "ln -s $dir_dotfiles/$file ~/$file"
-        ln -s $dir_dotfiles/$file ~/$file
-        echo
+if [ "$arg_dotfiles" = true ]; then
+    echo "================================================================================"
+    echo "installing dotfiles"
+    echo "--------------------------------------------------------------------------------"
+    dir_dotfiles=$dir/dotfiles
+    dir_dotfiles_old=$dir_dotfiles/old
+    dotfiles="$(ls -A $dir_dotfiles)"
+    if [ ! -d $dir_dotfiles_old ]; then
+        echo "mkdir $dir_dotfiles_old"
+        mkdir $dir_dotfiles_old
+        echo 
     fi
-done
-echo -e "done\n"
-
+    for file in $dotfiles; do
+        if [ "$file" != "old" ]; then
+            # If existing symlink
+            if [ -h ~/$file ]; then
+                echo "rm ~/$file"
+                rm ~/$file
+            # If existing regular file
+            elif [ -e ~/$file ]; then
+                echo "mv ~/$file $dir_dotfiles_old/"
+                mv ~/$file $dir_dotfiles_old/
+            fi
+            # Now, make the symlink
+            echo "ln -s $dir_dotfiles/$file ~/$file"
+            ln -s $dir_dotfiles/$file ~/$file
+            echo
+        fi
+    done
+    echo -e "done\n"
+fi
 
 #-----
 # git
 
-if [[ "$setup_git" = true ]]; then
+if [ "$arg_git" = true ]; then
     echo "================================================================================"
     echo "setting up git"
     echo "--------------------------------------------------------------------------------"
@@ -126,21 +222,3 @@ if [[ "$setup_git" = true ]]; then
     git config --global push.default simple
     echo -e "done\n"
 fi
-
-#-----
-# manual installs
-
-echo "================================================================================"
-echo "manual installs"
-echo "--------------------------------------------------------------------------------"
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo "OS: linux-gnu"
-    echo "Nothing to do"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "OS: OS X"
-    echo "You should install these apps manually: ShiftIt"
-else
-    echo "OS: unknown"
-    echo "Nothing to do"
-fi
-echo
